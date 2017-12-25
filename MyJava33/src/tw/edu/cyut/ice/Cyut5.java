ccb8a93fd01370ba2a35eb56d7560fab7dea5949
E517 <E517@E517-58>
2017-12-25 11:21:36 +0800

    201712251121

--- MyJava33/src/tw/edu/cyut/ice/Cyut5.java
+++ MyJava33/src/tw/edu/cyut/ice/Cyut5.java
@@ -1,10 +1,28 @@
 package tw.edu.cyut.ice;
 
+import java.awt.BorderLayout;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JTextArea;
 
 public class Cyut5 extends JFrame {
+	private JButton b1, b2, b3;
+	private JTextArea area;
+	
 	public Cyut5() {
 		super("My Window");
+		
+		b1 = new JButton("B1");
+		b2 = new JButton("B2");
+		b3 = new JButton("B3");
+		area = new JTextArea();
+		
+		setLayout(new BorderLayout());
+		add(b1, BorderLayout.NORTH);
+		add(b2, BorderLayout.NORTH);
+		add(b3, BorderLayout.NORTH);
+		add(area, BorderLayout.CENTER);
 		
 		setVisible(true);
 		setSize(640, 480);
