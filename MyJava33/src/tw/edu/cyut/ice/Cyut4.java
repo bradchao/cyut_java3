c1d42f2a33aab89290e58360f21d10fd3c34a97d
E517 <E517@E517-58>
2017-12-25 10:41:35 +0800

    201712251041

---
+++ MyJava33/src/tw/edu/cyut/ice/Cyut4.java
@@ -0,0 +1,38 @@
+package tw.edu.cyut.ice;
+
+public class Cyut4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+interface Shape {
+	double calLegth();
+	double calArea();
+}
+class Circle implements Shape {
+	public double calLegth() {
+		return 0;
+	}
+	public double calArea() {
+		return 0;
+	}
+}
+class Triangle implements Shape {
+	public double calLegth() {
+		return 0;
+	}
+	public double calArea() {
+		return 0;
+	}
+}
+class Rectangle implements Shape {
+	public double calLegth() {
+		return 0;
+	}
+	public double calArea() {
+		return 0;
+	}
+}
